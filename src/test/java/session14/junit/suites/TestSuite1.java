73442da6c46e23d92dbfb83e9c2f54164fb7bcdf
Oleg Sirko <oleg.sirko@orderin.co.za>
2022-08-25 17:56:41 +0300

    Junit Suites

---
+++ src/test/java/session14/junit/suites/TestSuite1.java
@@ -0,0 +1,14 @@
+package session14.junit.suites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        TestClass1.class,
+        TestClass2.class
+})
+
+public class TestSuite1 {
+}
---
+++ src/test/java/session14/junit/suites/TestSuite2.java
@@ -0,0 +1,13 @@
+package session14.junit.suites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        TestClass3.class
+})
+
+public class TestSuite2 {
+}

ebd1f60b2235fc9e3b8aa7b6c98cde49ba05ef10
DESKTOP-EJRFHLL\evolv <evolventzp@ukr.net>
2023-01-22 15:36:57 +0200

    Blousecolor changed

--- src/test/java/Onlineshop/shopItems.java
+++ src/test/java/Onlineshop/shopItems.java
@@ -2,7 +2,7 @@ package Onlineshop;
 
 public class shopItems {
 
-    private String blouse = "Розовая блуза1";
+    private String blouse = "Блуза совсем не розовая уже";
     private String phone = "Айфон 11";
 
     private int blousePrice = 100;
